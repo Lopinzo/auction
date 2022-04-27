26af16e7f925d2e1afdc1a93f8f87a603aaa877d
Filip Sáss <sassfili@fit.cvut.cz>
2022-04-27 20:30:14 +0200

    generate equals and hashcode

--- src/main/java/cz/kb/sassfili/auction/data/Bid.java
+++ src/main/java/cz/kb/sassfili/auction/data/Bid.java
@@ -1,9 +1,24 @@
 package cz.kb.sassfili.auction.data;
 
+import java.util.Objects;
+
 public class Bid {
     final Person person;
 
     public Bid(Person person) {
         this.person = person;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Bid bid = (Bid) o;
+        return person.equals(bid.person);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(person);
     }
 }
