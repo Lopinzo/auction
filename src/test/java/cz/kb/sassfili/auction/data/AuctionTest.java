fe9d0efe70c410081fc0cb2615abf07411dd3be0
Filip Sáss <sassfili@fit.cvut.cz>
2022-04-27 20:13:24 +0200

    add empty tests

--- src/test/java/cz/kb/sassfili/auction/data/AuctionTest.java
+++ src/test/java/cz/kb/sassfili/auction/data/AuctionTest.java
@@ -6,4 +6,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AuctionTest {
 
+
+    @Test
+    void auction() {
+    }
+
+    @Test
+    void isWinning() {
+    }
+
+    @Test
+    void getWinningBid() {
+    }
+
+    @Test
+    void getCurrentValue() {
+    }
 }
