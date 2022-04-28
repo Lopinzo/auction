de1a5ad4040fc74faadec216698b934beec7742b
Filip Sáss <sassfili@fit.cvut.cz>
2022-04-28 10:29:17 +0200

    add getAuction function mapped on GET

--- src/main/java/cz/kb/sassfili/auction/controllers/AuctionController.java
+++ src/main/java/cz/kb/sassfili/auction/controllers/AuctionController.java
@@ -3,6 +3,8 @@ package cz.kb.sassfili.auction.controllers;
 import cz.kb.sassfili.auction.data.Auction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuctionController {
@@ -11,5 +13,12 @@ public class AuctionController {
     @Autowired
     public AuctionController(Auction auction) {
         this.auction = auction;
+    }
+
+    @GetMapping
+    String getAuction(Model model){
+        model.addAttribute("currentValue", auction.getCurrentValue());
+        model.addAttribute("amIWinning", "Ne");
+        return "index";
     }
 }
